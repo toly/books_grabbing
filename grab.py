fed340b1f28532dd3b103605832da85af69ff874
toly <tolytrader@gmail.com>
2014-02-14 22:38:51 +0400

    modify options for argparse

--- grab.py
+++ grab.py
@@ -11,18 +11,42 @@ import argparse
 
 def main():
     """main function"""
-    pass
+
+    parser = create_argument_parser()
+    arguments = parser.parse_args()
+
+    try:
+        validate_args(arguments)
+    except GrabException as e:
+        print "Error: %s!" % e.message
+        print "For help run: ./grap.py -h"
+        return 1
 
 
 def create_argument_parser():
     """make ArgumentParser object with necesary options"""
     argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument("-p", "--output-pdf", type=str, help="file for result output pdf file (*.pdf)")
-    argument_parser.add_argument("-t", "--output-txt", type=str, help="file for result output text file (*.txt)")
-    argument_parser.add_argument("-n", "--number-pages", type=int, help="number pages for grab (default 1000)")
-    argument_parser.add_argument("-c", "--cmp-after", type=int, help="number of pages, after which the last two pages "
-                                                                     "will be compared and if they equal - stop")
+
+    argument_parser.add_argument("-t", "--title", type=str, required=True, help="book title")
+    argument_parser.add_argument("-f", "--formats", type=str, required=True, nargs="*",
+                                 help="need formats of output file", choices=["pdf", "txt"])
+    argument_parser.add_argument("-n", "--number-pages", type=int, help="number pages for grab (default 1000)",
+                                 default=1000)
+
+    help_for_cmp_after = "number of pages, after which the last two pages will be compared and if they equal - stop"
+    argument_parser.add_argument("-c", "--cmp-after", type=int, help=help_for_cmp_after, default=3)
+
     return argument_parser
+
+
+class GrabException(Exception):
+    pass
+
+
+def validate_args(args):
+    """validate command line rguments"""
+
+    raise GrabException('zzz')
 
 
 def grabbing_screen(number_pages=None, cmp_pages_after=None):
@@ -37,7 +61,7 @@ def grabbing_screen(number_pages=None, cmp_pages_after=None):
 
 
 def recognize_image(image):
-    """recocginze image and return text"""
+    """recoginze image and return text"""
     pass
 
 
